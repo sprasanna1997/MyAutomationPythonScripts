1a5b82cb56cfa25ded0fe48701092eff5f156e06
sprasanna1997 <sprasanna1997@gmail.com>
2024-06-23 00:09:33 +0530

    Selenium file added

--- FirstFile.py
+++ FirstFile.py
@@ -1,2 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+options=webdriver.ChromeOptions()
+options.add_experimental_option('detach',True)
+driver=webdriver.Chrome(options=options)
+driver.get('https://www.google.in')
 name='Prasanna'
 print('New Hello World '+name)
