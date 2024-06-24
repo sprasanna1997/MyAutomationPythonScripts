d7c8a2b25c963dcfefc8d3647c49cc73f6c43283
sprasanna1997 <sprasanna1997@gmail.com>
2024-06-25 01:25:22 +0530

    Adding Selenium practice link

--- FirstFile.py
+++ FirstFile.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.keys import Keys
 options=webdriver.ChromeOptions()
 options.add_experimental_option('detach',True)
 driver=webdriver.Chrome(options=options)
-driver.get('https://www.google.in')
+driver.get('https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php')
 name='Prasanna'
 print('New Hello World Komalii')
