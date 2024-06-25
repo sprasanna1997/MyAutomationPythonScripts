c51d7593e56fcdfc09e8a48ebe8de032d9f3cda6
sprasanna1997 <sprasanna1997@gmail.com>
2024-06-25 18:42:10 +0530

    Adding openpyxl package

--- FirstFile.py
+++ FirstFile.py
@@ -1,3 +1,4 @@
+import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,3 +8,18 @@ driver=webdriver.Chrome(options=options)
 driver.get('https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php')
 name='Prasanna'
 print('New Hello World Komalii')
+driver.close()
+path='D:/Automation/Openpyxl.xlsx'
+workbook=openpyxl.load_workbook(path)
+sheet=workbook.get_sheet_by_name('Sheet1')
+rows=sheet.max_row
+cols=sheet.max_column
+print(rows)
+print(cols)
+
+for r in range(1,rows+1):
+    for c in range(1,cols+1):
+        res=sheet.cell(row=r,column=c).value
+        print(res,end="                     ")
+
+    print()
