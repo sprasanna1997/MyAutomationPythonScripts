f726eabe7ca18506f999085a2455d582201c16f2
sprasanna1997 <sprasanna1997@gmail.com>
2024-06-27 19:27:40 +0530

    Automating Selenium Practise using Pytest Framework

--- test_PytestDemo.py
+++ test_PytestDemo.py
@@ -1,7 +1,35 @@
+import time
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
-def test_signin():
-    print("Signin Successful")
+class TestPractice:
 
-def test_signout():
-    print("SignOut Successful")
+    #Variable Name
+    name="Prasanna Senthilkumar"
+    email="ji@gmail.com"
+    address="80, Ashok Nagar \n Chennai \n Tamilnadu"
+    password="Dummypassowrd"
+    @pytest.fixture()
+    def setup(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option('detach',True)
+        self.driver= webdriver.Chrome(options=self.options)
+        self.driver.maximize_window()
+        yield
+        self.driver.close()
+    def test_element_textbox(self,setup):
+        self.wait=WebDriverWait(self.driver,10)
+        self.driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php")
+        self.wait.until(ec.element_to_be_clickable((By.XPATH,"//button[text()=' Elements']"))).click()
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//a[text()=' Text Box']"))).click()
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id,'fullname')]"))).send_keys(self.name)
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id,'email')]"))).send_keys(self.email)
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//textarea[contains(@id,'address')]"))).send_keys(self.address)
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id,'password')]"))).send_keys(self.password)
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, "// input[ @ value = 'Submit']"))).click()
+
+    def test_signout(self,setup):
+        print("SignOut Successful")
